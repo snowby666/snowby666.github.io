1a0c237031b7e785800519afc6fefc713eaee19b
snowby666 <giabao070206@gmail.com>
2023-11-09 23:57:36 +0700

    Modified

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ import Explorer from '../components/Explorer';
 import Bottombar from '../components/Bottombar';
 import Tabsbar from './Tabsbar';
 import styles from '../styles/Layout.module.css';
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 
 const useWidth = () => {
   const [width, setWidth] = useState(0); // default width, detect on server.
